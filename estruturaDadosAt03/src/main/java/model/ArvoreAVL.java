7205bd25eeec3d326007f95c02f611ad2d89fea2
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2025-03-23 12:55:23 -0300

    Update de licença.

--- estruturaDadosAt03/src/main/java/model/ArvoreAVL.java
+++ estruturaDadosAt03/src/main/java/model/ArvoreAVL.java
@@ -1,6 +1,18 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C) 2025 Spindola
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package model;
@@ -11,6 +23,11 @@ import model.IAnalisadorDesempenho;
 import model.IEstruturaDeDados;
 import model.No;
 import model.Palavras;
+
+/**
+ *
+ * @author Ozeias Campos
+ */
 
 public class ArvoreAVL implements IEstruturaDeDados {
 
